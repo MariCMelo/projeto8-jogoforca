95e91e9e503e759dc7195e5c31f690e2699c2bf9
Marilia <marilia.correa.melo@gmail.com>
2023-05-11 22:10:35 -0300

    Primeiro componente, componentização feita

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Game from "./Game";
+import Letters from "./Letters";
+import palavras from "./palavras";
+
+export default function App() {
+  return (
+    <div >
+    olá
+    <Game />
+    <Letters />
+    </div>
+  );
+}
+
+
---
+++ src/components/Letters.js
@@ -0,0 +1,8 @@
+export default function Letters(){
+    
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    return(
+
+        
+    )
+}
